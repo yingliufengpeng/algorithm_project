990a5a491012f04da961fe6ddaab0b36e6bacb22
yingliufengpeng <2906631584@qq.com>
2019-06-15 20:18:51 +0800

    'update'

--- src/stack/stack_test.go
+++ src/stack/stack_test.go
@@ -1,12 +1,11 @@
 package stack
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 var s = New()
-
 
 func TestStringPush(t *testing.T) {
 	s.Push("H")
@@ -40,15 +39,15 @@ func TestStuctPush(t *testing.T) {
 		name string
 		age  int
 	}
-	s.Push(&d{name:"a", age:10})
+	s.Push(&d{name: "a", age: 10})
 	fmt.Println(s.Peek())
-	s.Push(&d{name:"b", age:11})
+	s.Push(&d{name: "b", age: 11})
 	fmt.Println(s.Peek())
-	s.Push(&d{name:"c", age:12})
+	s.Push(&d{name: "c", age: 12})
 	fmt.Println(s.Peek())
-	s.Push(&d{name:"d", age:13})
+	s.Push(&d{name: "d", age: 13})
 	fmt.Println(s.Peek())
-	s.Push(&d{name:"e", age:14})
+	s.Push(&d{name: "e", age: 14})
 	fmt.Println(s.Peek())
 	fmt.Println("=======TestStuctPush=======")
 }
@@ -65,4 +64,3 @@ func TestStuctPop(t *testing.T) {
 	fmt.Println(s.Peek())
 	fmt.Println("=======TestStuctPop=======")
 }
-
